97cb848a6e4d036b617fd615b5e3ac506fd8cdf9
Will Dantas <126539225+dantaswilljpg@users.noreply.github.com>
2023-12-11 16:55:00 -0300

    Update Aluno.java

--- src/main/java/sistemaCaptura/user/Aluno.java
+++ src/main/java/sistemaCaptura/user/Aluno.java
@@ -6,9 +6,10 @@ import sistemaCaptura.conexao.Conexao;
 
 import java.util.List;
 
-public class Aluno extends Usuario{
+public class Aluno extends Usuario {
     static Conexao conexao = new Conexao();
-    public  Aluno(){}
+
+    public Aluno() {}
 
     public Aluno(Usuario usuario) {
         super(usuario.getIdUsuario(), usuario.getNome(), usuario.getEmail(), usuario.getSenha(), usuario.getFkInstituicao(), usuario.getFkTipoUsuario());
@@ -17,13 +18,30 @@ public class Aluno extends Usuario{
     public Aluno(Integer idUsuario, String nome, String email, String senha, Integer fkInstituicao, Integer fkTipoUsuario) {
         super(idUsuario, nome, email, senha, fkInstituicao, fkTipoUsuario);
     }
+
     public void opcaoAluno() {
-        JdbcTemplate con = conexao.getConexaoDoBanco();
-
-        System.out.println("Essa opção esta bloqueada para o seu nivel de usuario");
+        System.out.println("Essa opção está bloqueada para o seu nível de usuário");
     }
+
+    @Override
     public String toString() {
-        return "aluno{} " + super.toString();
+        return "Aluno{} " + super.toString();
     }
 
+    public void listarAlunos() {
+        JdbcTemplate con;
+        if (conexao.getDev()) {
+            con = conexao.getConexaoDoBancoMySQL();
+        } else {
+            con = conexao.getConexaoDoBancoSQLServer();
+        }
+
+        List<Usuario> alunos = con.query("SELECT * FROM usuario WHERE fkInstituicao = ? AND fkTipoUsuario = ?",
+                new BeanPropertyRowMapper<>(Usuario.class), getFkInstituicao(), 1);
+
+        System.out.println("Lista de Alunos:");
+        for (Usuario aluno : alunos) {
+            System.out.println("Nome: " + aluno.getNome());
+        }
+    }
 }

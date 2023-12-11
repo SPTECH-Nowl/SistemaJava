6ffd6c232eaf6dc6a80ea7167396d1cc4200c8ff
Will Dantas <126539225+dantaswilljpg@users.noreply.github.com>
2023-12-11 16:52:59 -0300

    Update Professor.java

--- src/main/java/sistemaCaptura/user/Professor.java
+++ src/main/java/sistemaCaptura/user/Professor.java
@@ -9,25 +9,35 @@ import java.util.List;
 
 public class Professor extends Usuario {
     static Conexao conexao = new Conexao();
-    List<Usuario>usuarios;
-    public  Professor(){ usuarios=  new ArrayList<>();}
+    List<Usuario> usuarios;
+
+    public Professor() {
+        usuarios = new ArrayList<>();
+    }
+
     public Professor(Usuario usuario) {
         super(usuario.getIdUsuario(), usuario.getNome(), usuario.getEmail(), usuario.getSenha(), usuario.getFkInstituicao(), usuario.getFkTipoUsuario());
-        usuarios=  new ArrayList<>();
+        usuarios = new ArrayList<>();
     }
 
     public Professor(Integer idUsuario, String nome, String email, String senha, Integer fkInstituicao, Integer fkTipoUsuario) {
         super(idUsuario, nome, email, senha, fkInstituicao, fkTipoUsuario);
     }
+
     public void opcaoProfessor() {
-        JdbcTemplate con = conexao.getConexaoDoBanco();
+        JdbcTemplate con;
+        if (conexao.getDev()) {
+            con = conexao.getConexaoDoBancoMySQL();
+        } else {
+            con = conexao.getConexaoDoBancoSQLServer();
+        }
 
         usuarios = con.query("SELECT * FROM usuario WHERE fkInstituicao = ? AND fkTipoUsuario=?",
-                new BeanPropertyRowMapper<>(Usuario.class),getFkInstituicao(), 3);
+                new BeanPropertyRowMapper<>(Usuario.class), getFkInstituicao(), 3);
         System.out.println("Lista de Professores");
 
-        for (Usuario usuario : usuarios){
-            System.out.println("Nome: "+ usuario.getNome());
+        for (Usuario usuario : usuarios) {
+            System.out.println("Nome: " + usuario.getNome());
         }
     }
 
@@ -38,5 +48,5 @@ public class Professor extends Usuario {
     @Override
     public String toString() {
         return "Professor{} " + super.toString();
-    }
+    }
 }
